7695e3b3090597382de20e3b54302c35805cf758
XJINE <xjine.andorelseif@gmail.com>
2019-10-19 10:53:59 +0900

    Update access level and add string format type property.

--- Assets/Packages/DateTimeUtil/DateTimeUtil.cs
+++ Assets/Packages/DateTimeUtil/DateTimeUtil.cs
@@ -1,34 +1,46 @@
 ﻿public static class DateTimeUtil
 {
     // NOTE:
-    // "using DateTimeUtil" is useful.
+    // "using DateTimeUtil;" is useful.
 
-    public static float DayToHour        = 24;
-    public static float DayToMinute      = 24 * 60;
-    public static float DayToSecond      = 24 * 60 * 60;
-    public static float DayToMillisecond = 24 * 60 * 60 * 100;
+    #region Field
 
-    public static float HourToDay         = 1 / 24;
-    public static float HourToMinute      = 60;
-    public static float HourToSecond      = 60 * 60;
-    public static float HourToMillisecond = 60 * 60 * 100;
+    public const float DayToHour        = 24;
+    public const float DayToMinute      = 24 * 60;
+    public const float DayToSecond      = 24 * 60 * 60;
+    public const float DayToMillisecond = 24 * 60 * 60 * 100;
 
-    public static float MinuteToDay         = 1 / 60 / 24;
-    public static float MinuteToHour        = 1 / 60;
-    public static float MinuteToSecond      = 60;
-    public static float MinuteToMillisecond = 60 * 100;
+    public const float HourToDay         = 1 / 24;
+    public const float HourToMinute      = 60;
+    public const float HourToSecond      = 60 * 60;
+    public const float HourToMillisecond = 60 * 60 * 100;
 
-    public static float SecondToDay         = 1 / 60 / 60 / 24;
-    public static float SecondToHour        = 1 / 60 / 60;
-    public static float SecondToMinute      = 1 / 60;
-    public static float SecondToMillisecond = 100;
+    public const float MinuteToDay         = 1 / 60 / 24;
+    public const float MinuteToHour        = 1 / 60;
+    public const float MinuteToSecond      = 60;
+    public const float MinuteToMillisecond = 60 * 100;
 
-    public static float MillisecondToDay     = 1 / 100 / 60 / 60 / 24;
-    public static float MillisecondToHour    = 1 / 100 / 60 / 60;
-    public static float MillisecondToMinute  = 1 / 100 / 60;
-    public static float MillisecondToSecond  = 1 / 100;
+    public const float SecondToDay         = 1 / 60 / 60 / 24;
+    public const float SecondToHour        = 1 / 60 / 60;
+    public const float SecondToMinute      = 1 / 60;
+    public const float SecondToMillisecond = 100;
 
-    public static float SytemTimeSecondNow
+    public const float MillisecondToDay     = 1 / 100 / 60 / 60 / 24;
+    public const float MillisecondToHour    = 1 / 100 / 60 / 60;
+    public const float MillisecondToMinute  = 1 / 100 / 60;
+    public const float MillisecondToSecond  = 1 / 100;
+
+    // NOTE:
+    // These Formats are defined as replaceable value.
+
+    public static string DateTimeFormatHMSF   = "HH:mm:ss:ff";
+    public static string DateTimeFormatYMDHMS = "yy/MM/dd HH:mm:ss";
+
+    #endregion Field
+
+    #region Property
+
+    public static float DateTimeNowInSecond
     {
         get
         {
@@ -39,4 +51,16 @@
                  + now.Second;
         }
     }
+
+    public static string DateTimeNowInHMSF
+    { 
+        get { return System.DateTime.Now.ToString(DateTimeFormatHMSF); }
+    }
+
+    public static string DateTimeNowInYMDHMS
+    {
+        get { return System.DateTime.Now.ToString(DateTimeFormatYMDHMS); }
+    }
+
+    #endregion Property
 }
