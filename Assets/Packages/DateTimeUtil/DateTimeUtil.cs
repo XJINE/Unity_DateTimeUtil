abcc35426de456f8cba0c27f2c6469d9e7b5e595
XJINE <umeda@team-lab.com>
2022-09-15 17:14:20 +0800

    Fix some bug and remove season functions.

--- Assets/Packages/DateTimeUtil/DateTimeUtil.cs
+++ Assets/Packages/DateTimeUtil/DateTimeUtil.cs
@@ -5,30 +5,33 @@
 
     #region Field
 
+    // CAUTION:
+    // 'f' is quite important to make a value as float.
+
     public const float DayToHour        = 24;
     public const float DayToMinute      = 24 * 60;
     public const float DayToSecond      = 24 * 60 * 60;
-    public const float DayToMillisecond = 24 * 60 * 60 * 100;
+    public const float DayToMillisecond = 24 * 60 * 60 * 1000;
 
-    public const float HourToDay         = 1 / 24;
+    public const float HourToDay         = 1f / 24;
     public const float HourToMinute      = 60;
     public const float HourToSecond      = 60 * 60;
-    public const float HourToMillisecond = 60 * 60 * 100;
+    public const float HourToMillisecond = 60 * 60 * 1000;
 
-    public const float MinuteToDay         = 1 / 60 / 24;
-    public const float MinuteToHour        = 1 / 60;
+    public const float MinuteToDay         = 1f / 60 / 24;
+    public const float MinuteToHour        = 1f / 60;
     public const float MinuteToSecond      = 60;
-    public const float MinuteToMillisecond = 60 * 100;
+    public const float MinuteToMillisecond = 60 * 1000;
 
-    public const float SecondToDay         = 1 / 60 / 60 / 24;
-    public const float SecondToHour        = 1 / 60 / 60;
-    public const float SecondToMinute      = 1 / 60;
-    public const float SecondToMillisecond = 100;
+    public const float SecondToDay         = 1f / 60 / 60 / 24;
+    public const float SecondToHour        = 1f / 60 / 60;
+    public const float SecondToMinute      = 1f / 60;
+    public const float SecondToMillisecond = 1000;
 
-    public const float MillisecondToDay     = 1 / 100 / 60 / 60 / 24;
-    public const float MillisecondToHour    = 1 / 100 / 60 / 60;
-    public const float MillisecondToMinute  = 1 / 100 / 60;
-    public const float MillisecondToSecond  = 1 / 100;
+    public const float MillisecondToDay     = 1f / 1000 / 60 / 60 / 24;
+    public const float MillisecondToHour    = 1f / 1000 / 60 / 60;
+    public const float MillisecondToMinute  = 1f / 1000 / 60;
+    public const float MillisecondToSecond  = 1f / 1000;
 
     // NOTE:
     // These Formats are defined as replaceable value.
@@ -46,82 +49,15 @@
         {
             var now = System.DateTime.Now;
 
-            return now.Hour   * HourToSecond
+            return now.Hour * HourToSecond
                  + now.Minute * MinuteToSecond
-                 + now.Second;
+                 + now.Second
+                 + now.Millisecond * MillisecondToSecond;
         }
     }
 
-    public static string DateTimeNowInHMSF
-    { 
-        get { return System.DateTime.Now.ToString(DateTimeFormatHMSF); }
-    }
-
-    public static string DateTimeNowInYMDHMS
-    {
-        get { return System.DateTime.Now.ToString(DateTimeFormatYMDHMS); }
-    }
+    public static string DateTimeNowInHMSF   => System.DateTime.Now.ToString(DateTimeFormatHMSF);
+    public static string DateTimeNowInYMDHMS => System.DateTime.Now.ToString(DateTimeFormatYMDHMS);
 
     #endregion Property
-
-    #region Season
-
-    public enum Season
-    {
-        Spring,
-        Summer,
-        Autumn,
-        Winter
-    }
-
-    public static Season SeasonFromDateTimeNowMonthInJapan
-    {
-        // NOTE:
-        // 12,  1,  2 = Winter
-        //  3,  4,  5 = Spring
-        //  6,  7,  8 = Summer
-        //  9, 10, 11 = Autumn
-
-        // EX:
-        // DateTimeNowMonth = 12 > 10 = Winter
-        //                  =  3 >  1 = Spring
-        //                  =  7 >  5 = Summer
-
-        get { return SeasonFromDateTimeNowMonth(-2); }
-    }
-
-    public static Season SeasonFromDateTimeNowMonth(int shiftMonth)
-    {
-        return SeasonFromMoneth(System.DateTime.Now.Month + shiftMonth);
-    }
-
-    public static Season SeasonFromMoneth(int month)
-    {
-        month = month % 12;
-
-        if (month <= 0) month = 12 + month;
-
-        switch (month)
-        {
-            case 1:
-            case 2:
-            case 3:
-                return Season.Spring;
-            case 4:
-            case 5:
-            case 6:
-                return Season.Summer;
-            case 7:
-            case 8:
-            case 9:
-                return Season.Autumn;
-            case 10:
-            case 11:
-            case 12:
-            default:
-                return Season.Winter;
-        }
-    }
-
-    #endregion Season
 }
--- README.md
+++ README.md
@@ -20,27 +20,27 @@ Multiply these parameters.
 public const float DayToHour        = 24;
 public const float DayToMinute      = 24 * 60;
 public const float DayToSecond      = 24 * 60 * 60;
-public const float DayToMillisecond = 24 * 60 * 60 * 100;
+public const float DayToMillisecond = 24 * 60 * 60 * 1000;
 
-public const float HourToDay         = 1 / 24;
+public const float HourToDay         = 1f / 24;
 public const float HourToMinute      = 60;
 public const float HourToSecond      = 60 * 60;
-public const float HourToMillisecond = 60 * 60 * 100;
+public const float HourToMillisecond = 60 * 60 * 1000;
 
-public const float MinuteToDay         = 1 / 60 / 24;
-public const float MinuteToHour        = 1 / 60;
+public const float MinuteToDay         = 1f / 60 / 24;
+public const float MinuteToHour        = 1f / 60;
 public const float MinuteToSecond      = 60;
-public const float MinuteToMillisecond = 60 * 100;
+public const float MinuteToMillisecond = 60 * 1000;
 
-public const float SecondToDay         = 1 / 60 / 60 / 24;
-public const float SecondToHour        = 1 / 60 / 60;
-public const float SecondToMinute      = 1 / 60;
-public const float SecondToMillisecond = 100;
+public const float SecondToDay         = 1f / 60 / 60 / 24;
+public const float SecondToHour        = 1f / 60 / 60;
+public const float SecondToMinute      = 1f / 60;
+public const float SecondToMillisecond = 1000;
 
-public const float MillisecondToDay     = 1 / 100 / 60 / 60 / 24;
-public const float MillisecondToHour    = 1 / 100 / 60 / 60;
-public const float MillisecondToMinute  = 1 / 100 / 60;
-public const float MillisecondToSecond  = 1 / 100;
+public const float MillisecondToDay     = 1f / 1000 / 60 / 60 / 24;
+public const float MillisecondToHour    = 1f / 1000 / 60 / 60;
+public const float MillisecondToMinute  = 1f / 1000 / 60;
+public const float MillisecondToSecond  = 1f / 1000;
 ```
 
 or use ``public static float DateTimeNowInSecond;`` property.
@@ -56,22 +56,4 @@ public static string DateTimeFormatYMDHMS = "yy/MM/dd HH:mm:ss";
 
 public static string DateTimeNowInHMSF;
 public static string DateTimeNowInYMDHMS;
-```
-
-### Season
-
-There are some property and methods to get Season.
-
-```csharp
-public enum Season
-{
-    Spring,
-    Summer,
-    Autumn,
-    Winter
-}
-
-public static Season SeasonFromDateTimeNowMonthInJapan
-public static Season SeasonFromDateTimeNowMonth(int shiftMonth)
-public static Season SeasonFromMoneth(int month)
 ```
